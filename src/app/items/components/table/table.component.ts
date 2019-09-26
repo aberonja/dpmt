807d91b3cdf219facba61e06ded8970ecb40135b
BraneOpacic3ap <brane.opacic@3ap.ch>
2019-09-26 13:50:10 +0200

    husky tweeks and first pre-hook test

--- src/app/items/components/table/table.component.ts
+++ src/app/items/components/table/table.component.ts
@@ -1,29 +1,31 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { OpenSidePanel } from '@uefa-dptm/shared/components/side-panel/state/side-panel.actions';
-import { ISidePanelState } from '@uefa-dptm/shared/components/side-panel/state/side-panel.model';
+import { Component } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { OpenSidePanel } from '@uefa-dptm/shared/components/side-panel/state/side-panel.actions'
+import { ISidePanelState } from '@uefa-dptm/shared/components/side-panel/state/side-panel.model'
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
-  isFetching: boolean = false;
+  isFetching: boolean = false
   items = [
     {
       id: 1,
-      checked: false
+      checked: false,
+      someValue: '21',
+      otherValue: '42',
     },
     {
       id: 2,
-      checked: false
-    }
-  ];
+      checked: false,
+    },
+  ]
 
   constructor(private _store: Store<{ sidePanel: ISidePanelState }>) {}
 
   openSidePanel(id: number) {
-    this._store.dispatch(OpenSidePanel({ payload: { type: 'item', id } }));
+    this._store.dispatch(OpenSidePanel({ payload: { type: 'item', id } }))
   }
 }
