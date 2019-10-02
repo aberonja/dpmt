d2ecdfa2e48e7194e7ecc2af92809f423b55f9f6
Aleksandar Beronja <aleksandar.beronja@3ap.ch>
2019-10-02 11:26:45 +0200

    add filters sidepanel

--- src/app/administration/venues/container/venues.component.ts
+++ src/app/administration/venues/container/venues.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { OpenSidePanel } from '@uefa-dptm/shared/components/side-panel/state/side-panel.actions';
+import { ISidePanelState } from '@uefa-dptm/shared/components/side-panel/state/side-panel.model';
 
 @Component({
   selector: 'app-venues',
   templateUrl: './venues.component.html',
-  styleUrls: ['./venues.component.scss']
+  styleUrls: ['./venues.component.scss'],
 })
 export class VenuesComponent implements OnInit {
+  constructor(private _store: Store<{ sidePanel: ISidePanelState }>) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openFilters() {
+    this._store.dispatch(
+      OpenSidePanel({
+        payload: { type: 'venueFilters', heading: 'Filter' },
+      })
+    );
   }
-
 }
