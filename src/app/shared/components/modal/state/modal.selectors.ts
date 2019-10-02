2460dca4a077593189e1d89c1aed7d981ba342df
Aleksandar Beronja <aleksandar.beronja@3ap.ch>
2019-10-02 11:45:12 +0200

    remove obsolete selector

--- src/app/shared/components/modal/state/modal.selectors.ts
+++ src/app/shared/components/modal/state/modal.selectors.ts
@@ -1,14 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { IModalState } from './modal.model';
 
-const getIsOpen = (state: IModalState) => state.isOpen;
 const getState = (state: IModalState) => state;
 
 const getModal = createFeatureSelector<IModalState>('modal');
-export const getIsOpenSelector = createSelector(
-  getModal,
-  getIsOpen
-);
 export const getStateSelector = createSelector(
   getModal,
   getState
